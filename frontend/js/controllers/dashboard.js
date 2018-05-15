965a53791f136d970a6b33146927c717615ecde4
Srujana <srujana.vemula@wohlig.com>
2018-05-15 18:58:26 +0530

    done

--- frontend/js/controllers/dashboard.js
+++ frontend/js/controllers/dashboard.js
@@ -2,5 +2,53 @@ myApp.controller('DashboardCtrl', function ($scope, TemplateService, NavigationS
     $scope.template = TemplateService.getHTML("content/dashboard.html");
     TemplateService.title = "Dashboard"; //This is the Title of the Website $scope.navigation
     $scope.navigation = NavigationService.getNavigation(); // This is the Title of the Website $scope.navigation= NavigationService.getNavigation();
-    $scope.TableData = [{"srno":"1","datetime":"3 Apr 2018 02:58:32 PM","username":"Withdrawal","desc":"Enim Nulla leo","amount":"85000","give":"Bitcoin Wallet","status":"Done"}];
+    $scope.TableData = [{
+        "srno": "1",
+        "datetime": "3 Apr 2018 02:58:32 PM",
+        "username": "Withdrawal",
+        "desc": "Enim Nulla leo",
+        "amount": "85000",
+        "give": "Bitcoin Wallet",
+        "status": "Done"
+    }, {
+        "srno": "2",
+        "datetime": "3 Apr 2018 02:58:32 PM",
+        "username": "Withdrawal",
+        "desc": "Enim Nulla leo",
+        "amount": "85000",
+        "give": "Bitcoin Wallet",
+        "status": "Done"
+    }, {
+        "srno": "3",
+        "datetime": "3 Apr 2018 02:58:32 PM",
+        "username": "Withdrawal",
+        "desc": "Enim Nulla leo",
+        "amount": "85000",
+        "give": "Bitcoin Wallet",
+        "status": "Done"
+    }, {
+        "srno": "4",
+        "datetime": "3 Apr 2018 02:58:32 PM",
+        "username": "Withdrawal",
+        "desc": "Enim Nulla leo",
+        "amount": "85000",
+        "give": "Bitcoin Wallet",
+        "status": "Done"
+    }, {
+        "srno": "5",
+        "datetime": "3 Apr 2018 02:58:32 PM",
+        "username": "Withdrawal",
+        "desc": "Enim Nulla leo",
+        "amount": "85000",
+        "give": "Bitcoin Wallet",
+        "status": "Done"
+    }, {
+        "srno": "6",
+        "datetime": "3 Apr 2018 02:58:32 PM",
+        "username": "Withdrawal",
+        "desc": "Enim Nulla leo",
+        "amount": "85000",
+        "give": "Bitcoin Wallet",
+        "status": "Done"
+    }];
 })
