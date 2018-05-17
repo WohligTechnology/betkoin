5e02aec5503375286fbb003b1a4d049095632deb
snehalthakare <snehal.thakare@wohlig.com>
2018-05-17 19:43:52 +0530

    changes

--- frontend/js/factories/navigation.js
+++ frontend/js/factories/navigation.js
@@ -1,8 +1,8 @@
 myApp.factory('NavigationService', function () {
     var navigation = [{
-            name: "Home",
-            classis: "active",
-            anchor: "home",
+            // name: "Home",
+            // classis: "active",
+            // anchor: "home",
             // subnav: [{
             //     name: "Subnav1",
             //     classis: "active",
