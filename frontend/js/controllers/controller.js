517e7b16a783a68516d1473e440cfcf1f3ac00fb
snehalthakare <snehal.thakare@wohlig.com>
2018-05-19 17:05:19 +0530

    changes for ui

--- frontend/js/controllers/controller.js
+++ frontend/js/controllers/controller.js
@@ -65,120 +65,34 @@ myApp.controller('HomeCtrl', function ($scope, TemplateService, NavigationServic
                 description: "Come see why this has always been one of the most popular poker variations!<br>1. beat the dealer by having a better hand.<br>2. win up to 1000: 1 on the optional 5 + 1 bonus bet "
             },
         ]
-        $scope.mySlides = [{
-                img: 'img/game4.jpg',
-                title: 'TeenPatti'
+
+        $scope.sportData = [{
+                img: 'img/Cricket.jpg',
+                title: 'Cricket',
+                description: " Bet as the action unfolds! We offer a multitude of different betting options across a huge number of matches and tournaments in cricket!.<ul> <li> Completed Match </li> <li> Tied Match </li> <li> 20 / 50 Over Runs </li> </ul> "
             },
             {
-                img: 'img/p1.jpg',
-                title: 'Texas Holdem Poker',
-                description: ""
-            }, {
-                img: 'img/r1.jpg',
-                title: 'Roulette',
-                description: " "
-            }, {
-                img: 'img/Baccarat.jpg',
-                title: 'Live Baccarat'
-            }, {
-                img: 'img/blackjack.jpg',
-                title: 'Live Blackjack'
-            }, {
-                img: 'img/money.png',
-                title: 'Live Money Wheel'
-            }, {
-                img: 'https://cdn.decent.bet/img/backgrounds/roulette.jpg',
-                title: 'Live Roulette'
-            }, {
-                img: 'img/game1.jpg',
-                title: 'Live Ultimate Texas Holdem'
+                img: 'img/Football.jpg',
+                title: 'Football',
+                description: "Sports betting is a billion dollar industry, and football is the most popular sports in the world to bet on.  We offer great value and great odds to bet on!For Football,such kinds of Bet Markets are available: "
             },
             {
-                img: 'img/exteme.png',
-                title: 'Live Extreme Texas Holdem '
-            },
-            {
-                img: 'img/casino.jpg',
-                title: 'Live Casino Holdem'
-            },
-
-            {
-                img: 'img/threecardpoker.jpg',
-                title: 'Live Three Card Poker'
-            },
-            {
-                img: 'img/triplecard.png',
-                title: 'Live Triple Card Poker '
-            },
-            {
-                img: 'img/7cardstud.jpg',
-                title: 'Live Caribbean Stud'
-            },
-
-
-            // {
-            //     img: 'https://cdn.decent.bet/img/backgrounds/craps.jpg',
-            //     title: 'Craps'
-            // },
-
-            // {
-            //     img: '../img/Casinowar.jpg',
-            //     title: 'Casino war'
-            // },
-            // {
-            //     img: '../img/Bigsixwheel.jpg',
-            //     title: 'Big six wheel'
-            // },
-            // {
-            //     img: '../img/7cardstud.jpg',
-            //     title: '7 card stud'
-            // },
-            // {
-            //     img: '../img/Slots.jpg',
-            //     title: 'Slots'
-            // },
-            // {
-            //     img: '../img/Bingo.jpg',
-            //     title: 'Bingo'
-            // },
-            // {
-            //     img: '../img/LotteryBetting.jpg',
-            //     title: 'Lottery Betting'
-            // },
-            // {
-            //     img: '../img/Dicegames.jpg',
-            //     title: 'Dice games'
-            // },
-            // {
-            //     img: '../img/Rummy.jpg',
-            //     title: 'Rummy'
-            // },
-            // {
-            //     img: '../img/Pinball.jpg',
-            //     title: 'Pinball'
-            // },
-            // {
-            //     img: '../img/Paigow.jpg',
-            //     title: 'Pai gow'
-            // },
-            // {
-            //     img: 'img/TexasHold.jpg',
-            //     title: 'Texas Holdem Bonus Poker'
-            // },
-            // {
-            //     img: 'img/dreamcatcher.png',
-            //     title: 'Dreamcatcher'
-            // }
-            // {
-            //     img: '../img/Keno.jpg',
-            //     title: 'Keno'
-            // }
-
-        ];
-
+                img: 'img/Tennis.jpg',
+                title: 'Tennis',
+                description: "Kings App enables you to bet in-play on the majority of matches, with the tennis odds changing to reflect the current state of the match.You can get a large number of special and exotic markets on most tournaments as well.For Tennis, such kinds of Bet Markets are available: <ul> <li> Match Odds </li> </ul> "
+            }, {
+                img: 'img/GreyhoundRacing.jpg',
+                title: 'Greyhound Racing',
+                description: "You’ll LOVE Greyhound Betting from Kings App. An app that offers everything you need to enjoy the sport to the  full."
+            }, {
+                img: 'img/HorseRacing.jpg',
+                title: 'Horse Racing',
+                description: " Experience the excitement of live horse racing and race replays on your device, wherever you are. Never miss a race again - it’ s like having a racing program in the palm of your hand!"
+            }
+        ]
         $timeout(function () {
             var mySwiper = new Swiper('.swiper-container', {
-                slidesPerView: 5,
+                slidesPerView: 3,
                 spaceBetween: 30,
                 pagination: {
                     el: '.swiper-pagination',
@@ -221,7 +135,7 @@ myApp.controller('HomeCtrl', function ($scope, TemplateService, NavigationServic
 
         $timeout(function () {
             var mySwiper = new Swiper('.swiper-container2', {
-                slidesPerView: 5,
+                slidesPerView: 3,
                 spaceBetween: 30,
                 pagination: {
                     el: '.swiper-pagination',
