27888ed288261fac81ef0c06f1baf47d85a8ed35
snehalthakare <snehal.thakare@wohlig.com>
2018-06-18 16:42:20 +0530

    changes

--- frontend/js/controllers/tutorials.js
+++ frontend/js/controllers/tutorials.js
@@ -3,5 +3,8 @@ myApp.controller('TutorialsCtrl', function ($scope, TemplateService, NavigationS
     TemplateService.title = "Tutorials";
     TemplateService.header1 = ""; //This is the Title of the Website $scope.navigation
     $scope.navigation = NavigationService.getNavigation(); // This is the Title of the Website $scope.navigation= NavigationService.getNavigation();
-
+    $scope.pouseVideo = function () {
+        var vid = document.getElementById("myVideo");
+        vid.onpause = function () {};
+    }
 })
